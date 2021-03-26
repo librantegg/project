c22f4bbb57087634e52f33ee949fe2a00c7d6881
Vova <example@mail.ru>
2021-03-26 11:15:28 +0300

    задача 2
    вывод вопросов в цикле, проверка введеной информации

--- project/js/script.js
+++ project/js/script.js
@@ -1,25 +1,59 @@
-"use strict";
+/* Задание на урок:
 
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-let num = 50;
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-while (num < 55) {
-    console.log(num);
-    num++;
-} 
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
+4) Потренироваться и переписать цикл еще двумя способами*/
 
-do { 
-    console.log(num);
-    num++;
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (numberOfFilms < 10) {
+     console.log("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms <= 30) {
+    console.log("Вы классический зритель");
+} else if (numberOfFilms > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+    
 }
-while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-    if ( i === 6) {
-        
-        continue;
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+for (let i = 0; i < numberOfFilms; i++) {
+    let  a = prompt('Один из последних просмотренных фильмов?', '');
+    while (a == null || a == '' || a.length > 50) {
+        console.log("Error");
+        a = prompt('Один из последних просмотренных фильмов?', '');
     }
-
-    console.log(i);
+    
+    let  b = prompt('На сколько вы оцените его?', '');
+    while (b < 1 || b > 10) {
+        console.log("Error");
+        b = prompt('На сколько вы оцените его?');
+    }
+    
+    personalMovieDB.movies[a] = b;
 }
+
+
+
+
+
+console.log(personalMovieDB);
