f15b29bc8da06fbc47bab8ec2954013a2e6eb720
Vova <vladimirbabkineho@gmail.com>
2021-04-06 12:25:34 +0300

    Начало ООП
    Object.create, Object.getPrototypeOf, Object.setPrototypeOf.

--- project/js/script.js
+++ project/js/script.js
@@ -1,96 +1,34 @@
-"use strict";
+/* "use strict";
 
-/* let a = 5,
-    b = a;
+let str = "some";
+let strObj = new String(str);
 
-b = b + 5;
 
-console.log(b);
-console.log(a);
+console.log(typeof (str));
+console.log(typeof (strObj));
 
-const obj = {
-    a : 5,
-    b: 1
-};
 
-const copy = obj;
+console.dir([1, 2, 3]); */
 
-copy.a = 10;
-
-console.log(copy);
-console.log(obj); */
-
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x:7,
-        y:4
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
     }
 };
 
-const newNumbers = copy(numbers);
 
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-console.log(newNumbers);
 
-console.log(numbers);
+const jonh = Object.create(soldier);
 
-const add = {
-   d: 17,
-   e: 20 
+/* 
+const jonh = {
+    health: 100
 };
 
-const clone = Object.assign({}, add);
+//jonh.__proto__ = soldier;
 
-clone.d = 20;
+Object.setPrototypeOf(jonh, soldier);*/
+jonh.sayHello(); 
 
-/* console.log(add);
-console.log(clone); */
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'adadadad';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ["a", "b"];
-const newAarray = [...array];
-console.log(newAarray);
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {...q};
