d683a133b669057f8a04ba2f2dbf30c3ce4e860d
Vova <vladimirbabkineho@gmail.com>
2021-04-30 21:54:25 +0300

    навигация по DOM-элементам

--- preview/preview/script.js
+++ preview/preview/script.js
@@ -1,36 +1,22 @@
-const btns = document.querySelectorAll('button'),
-    overlay = document.querySelector('.overlay');
+'use strict';
 
-/* btn.onclick = function() {
-    alert('click');
-};
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+//console.log(document.body.firstElementChild);
+//console.log(document.body.lastChild);
+ 
 
+//console.log(document.querySelector('#current').parentNode.parentNode);
+//console.log(document.querySelector('#current').parentElement);
 
-btn.onclick = function() {
-    alert('Second click');
-}; */
+//console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-let i = 0;
-const deleteElement = (e) => {
-    console.log(e.target);
-    console.log(e.type);
-   /*  i++;
-    if (i == 1) {
-        btn.removeEventListener('click', deleteElement);
-    } */
-};
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    
 
-//btn.addEventListener('click', deleteElement);
-//overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn=> {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-
-link.addEventListener('click',(event) => {
-    event.preventDefault();
-
-    console.log(event.target);
-});
+    console.log(node);
+}
