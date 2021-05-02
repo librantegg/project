00e853bb6d4f0bf29105cbfb43fbc7cd20ae388a
Vova <vladimirbabkineho@gmail.com>
2021-05-02 21:06:55 +0300

    Аттрибуты script

--- preview/preview/script.js
+++ preview/preview/script.js
@@ -1,24 +1,14 @@
 'use strict';
-// touchstart касание элта
-// touchmove если при касании начинается двигаться то при каждом движении
-// touchend при отрывании пальца
-// touchenter при наскальзывание на элемент при ведении пальца
-// touchleave палец при скольэении ушел за пределы елта
-// touchcancel когда точка соприкосновения больше не регистрируетс( прим в в браузере)
 
+const p = document.querySelectorAll('p');
+console.log(p);
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
 
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-
-        console.log('start');
-        console.log(e.targetTouches[0].pageX);
-    });
-});
-
-
-// touches
-// targetTouches
-// changedTouches
+loadScript("test.js");
+loadScript("some.js");
