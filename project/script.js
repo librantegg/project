f4cfe4d41c1a18a283c8856ba09e0969c54b54a8
Vova <vladimirbabkineho@gmail.com>
2021-06-04 22:13:46 +0300

    Rest, default parameters and their usage in food project

--- project/script.js
+++ project/script.js
@@ -1,38 +1,13 @@
-'use strict';
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
 
-class Rectangle {
-    constructor (height, width) {
-        this.height = height;
-        this.width = width;
-    }
+};
 
-    calcArea() {
-        return this.height * this.width;
-    }
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
+
 }
 
-
-class ColoredRectangleWithText extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width);
-        this.text = text;
-        this.bgColor = bgColor;
-    }
-
-    showMyProps() {
-        console.log(`ТЕкст : ${this.text}, color: ${this.bgColor}`);
-    }
-}
-
-const div = new ColoredRectangleWithText(25, 10, 'HelloWorld', 'red');
-
-div.showMyProps();
-console.log(div.calcArea());
-
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(20, 100);
-
-
-// console.log(long.calcArea());
-// console.log(square.calcArea());
-
+calcOrDouble(3);
