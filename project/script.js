b029c6b8a82d08d33b787aa68a1fc4d6aa82dc69
Vladimir <vladimirbabkineho@gmail.com>
2021-07-20 16:43:08 +0300

    promise

--- project/script.js
+++ project/script.js
@@ -1,13 +1,53 @@
-const log = function(a, b, ...rest) {
-    console.log(a, b, rest);
+// 'use strict';
 
+// console.log('Запрос данных...');
+
+const req = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('preparinf data...');
+
+        const product = {
+            name: 'TV',
+            price: 2000
+        };
+
+        resolve(product);
+    }, 2000);
+});
+const kek = setTimeout(() => console.log(req.state), 3000);
+
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//         }, 2000);
+//     });
+// }).then(data => {
+//     data.modify = true;
+//     return data;
+
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('Error');
+// }).finally(() => {
+//     console.log('Finally');
+// });
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
 };
+console.log(test);
+ test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
 
-log('basic', 'rest', 'operator', 'usage');
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('all');
+// });
 
-function calcOrDouble(number, basis = 2) {
-    console.log(number * basis);
-
-}
-
-calcOrDouble(3);
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('all');
+});
