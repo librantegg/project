f1bed629124db148b50c7d18b3f787820baa7b3c
Vladimir <vladimirbabkineho@gmail.com>
2021-07-26 14:42:31 +0300

    slider

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -136,7 +136,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 50000);
+   //const modalTimerId = setTimeout(openModal, 50000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -308,5 +308,66 @@ window.addEventListener('DOMContentLoaded', function () {
     //     .then(data => data.json())
     //     .then(res => console.log(res));
 
+
+    //slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current'),
+          total = document.querySelector('#total');
+
+
+    function getNumberOfSlides() {
+        if(slides.length < 10) {
+            total.textContent = `0${slides.length}`;
+        } else {
+            total.textContent = slides.length;
+        }
+    }
+
+
+    let slideIndex = 1;
+
+    function changeCurrent(n) {
+        if(n < 10) {
+            current.textContent = `0${n}`;
+        } else {
+            total.textContent = n;
+        }
+    }
+    
+
+    showSlides(slideIndex);
+    getNumberOfSlides();
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }   
+
+        slides.forEach(item => item.style.display ='none');
+
+        slides[slideIndex - 1].style.display = 'block';
+        changeCurrent(slideIndex);
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', (e) => {
+        plusSlides(-1);
+    });
+    
+    next.addEventListener('click', (e) => {
+        plusSlides(1);
+    });
+
+    
+    
 
 });
